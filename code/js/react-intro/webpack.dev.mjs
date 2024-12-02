6c47d0e0d35e01e51c5ecad400b82f69ac30d716
Pedro Felix <pmhsfelix@gmail.com>
2024-12-02 17:50:55 +0000

    changes proxy config to allow proxying of SSE

--- code/js/react-intro/webpack.dev.mjs
+++ code/js/react-intro/webpack.dev.mjs
@@ -1,22 +1,42 @@
-import { server } from "typescript";
+import { server } from 'typescript';
 
 export default {
-    mode: 'development',
-    devServer: {
-      historyApiFallback: true,
-      port: 3000,
-    },
-    resolve: {
-      extensions: ['.js', '.ts', '.tsx'],
-    },
-    plugins: [],
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
+  mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+    compress: false,
+    proxy: [
+      {
+        context: ['/api'],
+        target: 'http://localhost:8180',
+        onProxyRes: (proxyRes, req, res) => {
+          console.log('onProxyRes');
+          proxyRes.on('close', () => {
+            console.log('on proxyRes close');
+            if (!res.writableEnded) {
+              res.end();
+            }
+          });
+          res.on('close', () => {
+            console.log('on res close');
+            proxyRes.destroy();
+          });
         },
-      ],
-    },
-  };
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
